e2a0f83dcaf2d7c5524d14c22cb691596f4951fe
seno zacky <senozacky2006@gmail.com>
2023-12-03 15:11:25 +0700

    add stake page

--- mypages/nexus-stake.tsx
+++ mypages/nexus-stake.tsx
@@ -9,7 +9,7 @@ class NexusStake extends Component{
             <div className="mt-3 pb-3 container" style={{ backgroundColor: "#191F2D" }}>
                 <div className="row justify-content-center text-white">
                     <div className="col-12 text-center pt-3">
-                        <p className="text-white" style={{fontFamily: "Inter", fontSize: "25px", backgroundColor: "#242F38"}}>Stake Pool</p>
+                        <p className="text-white" style={{fontSize: "25px", backgroundColor: "#242F38"}}>Stake Pool</p>
                     </div>
                 </div>
                 <div className="row align-items-center">
@@ -17,10 +17,10 @@ class NexusStake extends Component{
                         <img src={stakeImg.src} className="px-3 py-3 img-fluid" alt="Stake" style={{backgroundColor: "#242F38"}} />
                     </div>
                     <div className="col-md-3 text-center mt-3 mt-md-0 ms-md-2 py-3" style={{backgroundColor: "#242F38"}}>
-                        <p className="text-white" style={{fontFamily:"Inter", fontSize:"20px"}}>Stake $NXS and Earn $NXS</p>
-                        <label className="text-white" htmlFor="numberInput" style={{fontFamily:"Inter", fontSize:"18px"}}>Amount Stake</label>
+                        <p className="text-white" style={{fontSize:"20px"}}>Stake $NXS and Earn $NXS</p>
+                        <label className="text-white" htmlFor="numberInput" style={{fontSize:"18px"}}>Amount Stake</label>
                         <NumberForm />
-                        <button className="my-md-3 px-md-3 my-2 btn px-5 text-white" style={{backgroundColor: "#59BE43", borderRadius: "15px"}}>Stake</button>
+                        <button className="my-md-3 px-md-3 my-2 btn px-5 px-md-5 text-white" style={{backgroundColor: "#59BE43", borderRadius: "15px"}}>Stake</button>
                         <p className="text-white">
                             Total staked
                             <br />
