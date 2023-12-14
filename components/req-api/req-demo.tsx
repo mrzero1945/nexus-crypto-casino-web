23c6ede7030b8f7ce5b06de45f9c5b5f24828e85
seno zacky <senozacky2006@gmail.com>
2023-12-14 17:31:07 +0700

    add api prototype

--- components/req-api/req-demo.tsx
+++ components/req-api/req-demo.tsx
@@ -15,6 +15,45 @@ class NexusReq extends Component<{}, RequestState> {
             error: null
         };
     }
+
+    sendPostRequest = async () => {
+        this.setState({isLoading: true});
+        try {
+            const response = await fetch('http://192.168.1.100:8080/command', {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({command: 'some_command'}),
+            });
+            if(!response.ok){
+                throw new Error(`Error: ${response.status}`);
+            }
+            const data = await response.text();
+            this.setState({response: data, isLoading:false});
+        } catch(error:any){
+            this.setState({error: error.message, isLoading:false });
+        }
+    }
+    render() {
+        const { response, isLoading, error } = this.state;
+
+        if (isLoading) {
+            return <div>Loading...</div>;
+        }
+
+        if (error) {
+            return <div>Error: {error}</div>;
+        }
+
+        return (
+            <div>
+                Response: {response}
+            </div>
+        );
+    }
     
 }
+
+export {NexusReq};
 
