d1943bb98eef3b50fda62c71c3128d6dff6fb6ef
seno zacky <senozacky2006@gmail.com>
2023-12-25 00:43:24 +0700

    add history feature

--- mypages/nexus-chatbot.tsx
+++ mypages/nexus-chatbot.tsx
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 const {GoogleGenerativeAI} = require("@google/generative-ai");
 // api
 const genAI = new GoogleGenerativeAI("AIzaSyAuO775Pfw5Mtqb9dF6Y--_0clihZzZDOI");
+
+type ChatHistoryItem = {
+  role: "user" | "model";
+  parts: string;
+};
+
+
+interface ChatOptions {
+  history: {
+    role: "user" | "model";
+    parts: string;
+  }[];
+  generationConfig: {
+    maxOutputTokens:number
+  };
+}
 
 
 interface NexusChatBotState {
 
   newMessage: string;
   combinedArray: string[];
+  chatOptions: ChatOptions;
 }
 
 class NexusChatBot extends Component<{}, NexusChatBotState> {
@@ -16,7 +34,13 @@ class NexusChatBot extends Component<{}, NexusChatBotState> {
     super(props);
     this.state = {
       newMessage: "",
-      combinedArray: []
+      combinedArray: [],
+      chatOptions : { history: [
+        {role: "user", parts: "jika ada yang menanyakan, namamu, jawab bahwa kau 'Nexus Bot' dan aku dikembangkan oleh 'Dani Hermawan'"},
+        {role: "model", parts: "ok, boss"}
+      ],
+      generationConfig: {maxOutputTokens: 2048} 
+    }
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -24,8 +48,30 @@ class NexusChatBot extends Component<{}, NexusChatBotState> {
   }
 
   async chatBot(prompt: string){
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const result = await model.generateContent(prompt);
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      }, {
+        category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+    ];
+    const model = genAI.getGenerativeModel({ model: "gemini-pro", safetySettings });
+    const chat = model.startChat(this.state.chatOptions);
+    const result = await chat.sendMessage(prompt);
     const response = await result.response;
     const text = response.text();
     return text;
@@ -37,17 +83,17 @@ class NexusChatBot extends Component<{}, NexusChatBotState> {
         <h3 className="text-center">Nexus ChatBot Beta</h3>
         <div className="row d-flex justify-content-center ">
           <div className="col-md-12">
-          <ul className="col-10 col-md-auto list-group mx-auto" style={{ maxWidth: "400px", height: "400px", overflowY: "scroll", backgroundColor: "#191F2D" }}>
+          <ul className="col-10 col-md-auto list-group mx-auto" style={{ width: "500px", height: "600px", overflowY: "scroll", backgroundColor: "#191F2D" }}>
             {this.state.combinedArray.map((message, index) => (
-                <div className={`${index % 2 ? "d-flex justify-content-end" : "d-flex justify-content-start"} row`}>
-                <div className={`col-auto`}>
-                    <li key={index} className={`px-md-3 my-3 mx-3 mx-md-3 my-md-3 list-group-item`} style={{ maxWidth: "wrap-content", maxHeight: "wrap-content", borderRadius: "15px" }}>
-                    {message}
-                    </li>
-                </div>
+                <div className={`row d-flex ${(index + 1) % 2 === 0 ? "justify-content-end" : "justify-content-start"}`}>
+                  <div className={`col-8`}>
+                      <li key={index} className={`list-group-item mx-md-3 my-md-2`} style={{ maxWidth: "wrap-content", maxHeight: "wrap-content", borderRadius: "15px" }}>
+                      {message}
+                      </li>
+                  </div>
                 </div>
             ))}
-</ul>
+          </ul>
 
           </div>
           <div className="col-md-4 col-10 mt-2 mt-md-2">
@@ -83,9 +129,19 @@ class NexusChatBot extends Component<{}, NexusChatBotState> {
     // Setelah pesan berhasil dikirim, tambahkan ke daftar pesan.
     if (this.state.newMessage !== "" && this.state.newMessage.replace(/^\s+/, "").length !== 0 && this.state.newMessage.length !== 300) {
         const botResponse = await this.chatBot(this.state.newMessage);
+        const newHistory:ChatHistoryItem[] = [
+          { role: "user", parts:  this.state.newMessage.replace(/^\s+/, "")},
+          { role: "model", parts: botResponse}
+          ]
+        ;
         this.setState({
           newMessage: "",
-          combinedArray: [...this.state.combinedArray, this.state.newMessage.replace(/^\s+/, ""), botResponse]
+          combinedArray: [...this.state.combinedArray, this.state.newMessage.replace(/^\s+/, ""), botResponse],
+           // Update chatOptions with the merged history
+          chatOptions: {
+            ...this.state.chatOptions,
+            history: [...this.state.chatOptions.history, ...newHistory],
+          }
         });
       }
       
